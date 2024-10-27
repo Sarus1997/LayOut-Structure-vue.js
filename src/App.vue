4678fa4d2db83cf036bee2c4b3037f62ed533c54
Sarus1997 <saharat.rus@gmail.com>
2024-10-27 14:35:54 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,8 @@
 <script setup>
-
-// ไม่ต้องใช้งาน routes และ currentView เนื่องจากเราจะให้ Vue Router จัดการ routing
-
-
+import Navbar from './components/NavbarPage.vue'
 </script>
 
 <template>
-  <div class="navbar">
-    <div class="logo">Logo</div>
-    <ul>
-      <li><a href="#/" class="nav-link">Home</a></li>
-      <li><a href="#/about" class="nav-link">About</a></li>
-      <li><a href="#/story" class="nav-link">Story</a></li>
-    </ul>
-  </div>
-
-  <!-- ใช้ <router-view /> สำหรับแสดง component ตามเส้นทาง -->
+  <Navbar />
   <router-view />
 </template>
-
-<style>
-/* ใส่สไตล์ได้ตามปกติ */
-</style>
---
+++ src/components/NavbarPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="navbar">
+    <div class="logo">Logo</div>
+    <ul>
+      <li><a href="#/" class="nav-link">Home</a></li>
+      <li><a href="#/about" class="nav-link">About</a></li>
+      <li><a href="#/story" class="nav-link">Story</a></li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+</script>
