c2decb66a5ca1b83a646889e570db8ddf1ef50c8
S Suwannapaond <saharat.rus@gmail.com>
2024-11-20 14:52:57 +0700

    update

--- src/main.js
+++ src/main.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-// main.js
->>>>>>> ae8300257790a15fc0d6dc12c58b8f29857380d3
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './routes/router';
